523e4e7bb07a59c8dbb387d91d5d39c1ba945bda
Thenmozhi2810 <167305519+Thenmozhi2810@users.noreply.github.com>
2026-06-12 20:20:01 +0530

    Add files via upload
    Day2 Assignment

---
+++ Assignment1.js
@@ -0,0 +1,37 @@
+
+// Primitive Data Types
+
+let userName="Abi"
+console.log(userName)
+console.log(typeof userName)
+
+let organization="Testleaf"
+console.log(organization)
+console.log(typeof organization)
+
+let contactNumber=1234567895
+console.log(contactNumber)
+console.log(typeof contactNumber)
+
+let knowsAutomation=true
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation)
+
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
+
+
+// var vs let vs const
+
+var browser
+const browserVersion="chrome"
+function getBrowserVersion()
+{
+    if(browser==="chrome")
+    {
+        console.log("The browser is chrome")
+    }
+    console.log("The browser is chrome")
+}
+getBrowserVersion()
