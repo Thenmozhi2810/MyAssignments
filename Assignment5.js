ca37a72afa036a2b1ad417c9c696fc7a12fc88d3
Thenmozhi <167305519+Thenmozhi2810@users.noreply.github.com>
2026-06-16 09:31:28 +0530

    Add files via upload
    Week 2 day 1 Assignment

---
+++ Assignment5.js
@@ -0,0 +1,9 @@
+
+const numbers=[10,15,20,25,30]   // Variable Declaration Initialization
+let sum=0                        
+for(const num of numbers)   // for-loop --> Used to iterate the numbers
+{
+        sum+=num;  // The added value is stored in the variable sum
+}
+console.log("The total sum of array present in the array is", sum)    // The final value is printed and displayed
+
