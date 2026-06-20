761a6fd9b9c1dfa50b94103f9684333fee2d2baa
Thenmozhi <167305519+Thenmozhi2810@users.noreply.github.com>
2026-06-20 20:01:29 +0530

    Add files via upload
    Assignment 4

---
+++ Assignment4.js
@@ -0,0 +1,11 @@
+
+// Write a JavaScript program to find the total sum of all numbers present in an array
+
+const numbers=[10,15,20,25,30]   // Variable Declaration Initialization
+let sum=0                        
+for(const num of numbers)   // for-loop --> Used to iterate the numbers
+{
+        sum+=num;  // The added value is stored in the variable sum
+}
+console.log("The total sum of array present in the array is", sum)    // The final value is printed and displayed
+
