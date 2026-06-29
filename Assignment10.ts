f3b27d78a8b2139349f263c28ca5b2bdc465ea40
Thenmozhi <167305519+Thenmozhi2810@users.noreply.github.com>
2026-06-29 13:57:04 +0530

    Add files via upload

---
+++ Assignment10.ts
@@ -0,0 +1,21 @@
+
+let testExecutionSummary={
+    suiteName:"Amazon",
+    totalTests:5,
+    passedTest:3,
+    failedTest:2,
+    executionTime:6000
+}
+console.log(testExecutionSummary)
+
+let failedTest=0
+
+    if(failedTest===0)
+    {
+        console.log("Execution Successful")
+    }
+    else
+    {
+        console.log("Execution Completed with failures")
+    }
+
