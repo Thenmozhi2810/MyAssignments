1d41ed98a5dfa8bfda3be063e15af8c33479b94a
Thenmozhi <167305519+Thenmozhi2810@users.noreply.github.com>
2026-07-24 15:27:41 +0530

    Week_5 Day_4 Classroom Assignment

---
+++ Week5_Classroom_Assignment.ts
@@ -0,0 +1,23 @@
+
+// Classroom Assignment ---> Demonstrating Polymorphism
+
+class Demonstrate
+{
+    reportStep(msg:string, status:string, snap?:boolean) : void
+    {
+        if(snap)
+    {
+        console.log( msg  +  " which is updated to the "  +  status  +  " and the statement is "  +  snap)
+    }
+    else
+    {
+        console.log( msg  +  " has become "  +  status  +  " nowadays")
+    }
+    }
+
+}
+
+const ObjDemo = new Demonstrate()
+ObjDemo.reportStep("Java is a Programming language", "latest version", true )
+ObjDemo.reportStep("Automation testing", "popular")
+
